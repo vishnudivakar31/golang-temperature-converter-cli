1265a41ffefe9f8532fc40da214beeb31a8cc99e
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-07-22 13:11:50 -0400

    Completed module

--- main.go
+++ main.go
@@ -24,8 +24,29 @@ func main() {
 	originUnit = strings.ToUpper(os.Args[1])
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+		_, err = fmt.Scanln(&originValue)
+
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		if originUnit == "F" {
+			convertToCelsius(originValue)
+		}
+
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
+
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		shouldConvertAgain = strings.ToUpper(shouldConvertAgain)
+		shouldConvertAgain = strings.TrimSpace(shouldConvertAgain)
 
 		if shouldConvertAgain != "Y" {
 			fmt.Println("Good bye!")
